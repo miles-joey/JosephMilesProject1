09193c9c3163b272a9b62de0f2caefd133ffe831
miles-joey <miles.joey@hotmail.com>
2023-06-06 13:21:52 -0400

    apparently wern't commited last week

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//checking that it is attached
 console.log("yes")
 
 const mobileButton = document.querySelector('.hamburger')
@@ -6,4 +6,6 @@ mobileButton.addEventListener('click',function(){
     const mobileMenu = document.querySelector('.mobileNav')
 mobileMenu.style.left='0'
 console.log('click is working')
+//checking that click is working
+//managed to get it to push out from the right
 })
